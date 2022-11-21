cfba4c6c9a0b3be30e4bd05a5275a3765b5f2f27
Gustavo Junqueira Barbosa <gustavojunqueirab@gmail.com>
2022-11-21 01:10:01 -0300

    Login finished

--- src/App.js
+++ src/App.js
@@ -1,8 +1,26 @@
 import React from "react";
+import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Footer from "./Components/Footer";
+import Header from "./Components/Header";
+import Home from "./Components/Home";
+import Login from "./Components/Login/Login";
+import { UserStorage } from "./UserContext";
 
 function App() {
   return (
-   <div>APP REACT</div>
+    <div>
+      <BrowserRouter>
+        <UserStorage>
+          <Header />
+          <Routes>
+            <Route path="/" element={<Home />}></Route>
+            <Route path="/login/*" element={<Login />}></Route>
+          </Routes>
+          <Footer />
+        </UserStorage>
+      </BrowserRouter>
+    </div>
   );
 }
 
