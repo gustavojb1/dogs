63bfed26c16fcce4c2f6d0c49bd9207153776ea4
Gustavo Junqueira Barbosa <gustavojunqueirab@gmail.com>
2022-11-23 00:22:11 -0300

    finished

--- src/Components/Photo/PhotoComents.js
+++ src/Components/Photo/PhotoComents.js
@@ -1,7 +1,39 @@
 import React from "react";
+import { UserContext } from "../../UserContext";
+import PhotoComentsForm from "./PhotoComentsForm";
+import styles from "./PhotoComents.module.css";
 
-const PhotoComents = () => {
-  return <div>PhotoComents</div>;
+const PhotoComents = (props) => {
+  const [comments, setComments] = React.useState(() => props.comments);
+  const commentsSection = React.useRef(null);
+  const { login } = React.useContext(UserContext);
+
+  React.useEffect(() => {
+    commentsSection.current.scrollTop = commentsSection.current.scrollHeigth;
+  }, [comments]);
+
+  return (
+    <>
+      <ul
+        ref={commentsSection}
+        className={`${styles.comments} ${props.single ? styles.single : ""}`}
+      >
+        {comments.map((comment) => (
+          <li key={comment.comment_ID}>
+            <b>{comment.comment_author}:</b>
+            <span>{comment.comment_content}</span>
+          </li>
+        ))}
+      </ul>
+      {login && (
+        <PhotoComentsForm
+          single={props.single}
+          id={props.id}
+          setComments={setComments}
+        />
+      )}
+    </>
+  );
 };
 
 export default PhotoComents;
